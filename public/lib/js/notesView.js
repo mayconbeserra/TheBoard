aefc3f8e8181d0e77a7d67fb44eef7d1cb873ca1
Maycon Beserra <mayconbeserra@hotmail.com>
2015-10-10 16:11:36 -0300

    Make the notes view to call the node API

--- public/lib/js/notesView.js
+++ public/lib/js/notesView.js
@@ -3,20 +3,19 @@
   var theModule = angular.module("notesView", []);
 
   theModule.controller("notesViewController",
-    ["$scope", "$window", "$http"
-      function ($scope) {
-        console.log('test');
+    ["$scope", "$window", "$http",
+      function ($scope, $window, $http) {
         $scope.notes = [];
 
         //get the categoryName ab
-        var urlParts = $window.location.pathname.split('/');
+        var urlParts = $window.location.pathname.split("/");
         var categoryName = urlParts[urlParts.length - 1];
-
+        console.log("test");
         var notesUrl = "/api/notes/" + categoryName;
         $http.get(notesUrl)
-          .then(function (data) {
+          .then(function (result) {
             //sucess
-            $scope.notes = result.data;
+            $scope.notes = result.data.notes;
           }, function (err) {
             //error
             alert(err);
